26fc24c03b57ca852e61ce69afd03ce5f048e057
mugimugi <okadadaikichi22@gmail.com>
2024-12-15 02:14:56 +0900

    update_test

--- pages/row/index.vue
+++ pages/row/index.vue
@@ -14,8 +14,9 @@ useHead({
   <div>
     <h3 class="rowTitle">row index</h3>
     <div class="pageIntro">
+      <p class="mb20">HOME / row / index</p>
       <p class="center">
-        ここはrow階層のindex、<br />子要素にindexファイル作ると親のrowファイルではなく子要素のindexが適用されるようです。<br />親のrowファイルは同名ディレクトリ以下の共通パーツに
+        ここはrow階層のindex、<br />子要素にindexファイル作ると親のrowファイルではなく子要素のindexが適用されるようです。リダイレクトみたいな感じ。<br />親のrowファイルは同名ディレクトリ以下の共通パーツに
       </p>
     </div>
   </div>
--- pages/row/title1.vue
+++ pages/row/title1.vue
@@ -14,6 +14,7 @@ useHead({
   <div>
     <h3 class="rowTitle">title1</h3>
     <div class="pageIntro">
+      <p class="mb20">HOME / row / title1</p>
       <p class="center">ここはrow階層のtitle1</p>
     </div>
   </div>
